98f2ce44feba3ce51b618f288226ac8d71ec4139
Douglous Sobei <douglousmangoyi@gmail.com>
2022-09-13 14:00:45 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include "main.h"
+
 /**
  * main -print putchar
- * return: always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }	
