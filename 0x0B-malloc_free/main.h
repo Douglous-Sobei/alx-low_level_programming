035f1537037182dd2d025eae4ef28f855f75ded7
Douglous Sobei <douglousmangoyi@gmail.com>
2022-10-03 22:23:54 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
