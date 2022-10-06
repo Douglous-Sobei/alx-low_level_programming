6427a54cef3a1fbe5b0418e1d25a83c7c1557b88
Douglous Sobei <douglousmangoyi@gmail.com>
2022-10-06 12:58:17 -0700

    My first commit

--- 0x0C-more_malloc_fre/0-malloc_checked.c
+++ 0x0C-more_malloc_fre/0-malloc_checked.c
@@ -1,21 +1,18 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
  *
- * Return: a pointer to the allocated memory
+ * Return: A pointer to the allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *mem = malloc(b);
 
-	ptr = malloc(b);
-
-	if (ptr == NULL)
+	if (mem == NULL)
 		exit(98);
 
-	return (ptr);
+	return (mem);
 }
-
--- 0x0C-more_malloc_fre/main.h
+++ 0x0C-more_malloc_fre/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Douglous Sobei
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0C-more_malloc_free directory.
+ */
+
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
