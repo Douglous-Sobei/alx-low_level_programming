87374cfd860da7f5e641be91ac777c76bb4d4569
Douglous Sobei <douglousmangoyi@gmail.com>
2023-01-08 11:58:15 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int a;
+	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
@@ -21,7 +21,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(hash_table);
 		return (NULL);
 	}
-	for (a = 0; i < size; i++)
-		hash_table->array[a] = NULL;
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
 	return (hash_table);
-}
